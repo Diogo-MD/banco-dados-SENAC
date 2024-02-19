220b5b4ad66ca675c6c48ebddb72da924950d9d3
Diogo-MD <diogomdossantos@gmail.com>
2024-02-19 20:47:07 -0300

    Atualização Aula

--- aulas/04/cria_db_compras.sql
+++ aulas/04/cria_db_compras.sql
@@ -1,1 +1,6 @@
-CREATE DATABASE compras;
+CREATE TABLE Pedido (
+    PedidoID INT PRIMARY KEY,
+    ClienteID INT,
+    DataPedido DATE,
+    FOREIGN KEY (ClienteID) REFERENCES Cliente (ClienteID)
+)
