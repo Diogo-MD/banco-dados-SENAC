9de900d9d89276d0a2b4b130f26a3102e8423281
Diogo-MD <diogomdossantos@gmail.com>
2024-02-27 20:23:47 -0300

    Atualização SQL

--- aulas/10/DQL- gestaoacademica.sql
+++ aulas/10/DQL- gestaoacademica.sql
@@ -14,3 +14,29 @@ FROM
 WHERE
     matricula > 5
 ORDER BY nome;
+
+-- Operadores Matemáticos e Lógicos:
+
+SELECT * FROM disciplina;
+
+SELECT nome, horas
+FROM disciplina
+WHERE horas >= 40 AND horas <= 50;
+
+-- Aliases para Melhor Legibilidade
+
+SELECT nome, horas AS carga_horaria
+FROM disciplina;
+
+-- Exiba da tabela professor, todos os professores por ordem alfabetica
+-- Onde o professor tenha ID maior do que 2 e menor do que 6
+-- Não exibindo a especialização e mudando a coluna "Nome" para "Nome_Professor"
+
+SELECT id, nome AS Nome_Professor
+FROM professor
+WHERE ID > 2 AND ID <6
+ORDER BY nome;
+
+-- Exemplo; Contar quantas disciplinas existem
+SELECT * 
+FROM disciplina;
