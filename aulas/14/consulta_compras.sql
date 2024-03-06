b7099dbc3a253de9c74f4a2dfd58dc06bd31869d
Diogo-MD <diogomdossantos@gmail.com>
2024-03-05 22:14:08 -0300

    Atualização SQL

--- aulas/14/consulta_compras.sql
+++ aulas/14/consulta_compras.sql
@@ -12,6 +12,27 @@ FROM pedido
 GROUP BY ClienteID;
 
 -- SELECIONAR O TOTAL DE PRODUTOS POR PEDIDO
-SELECT pedidoId, COUNT(*) AS QUANTIDADE
+SELECT pedidoId, COUNT(*) AS Quantidade_Pedidos
 FROM itenspedido
 GROUP BY pedidoId;
+
+-- SELECIONAR PRODUTOS MAIS VENDIDOS
+SELECT ProdutoId, SUM(quantidade) AS MaisVendidos
+FROM itensPedido
+GROUP BY ProdutoId
+ORDER BY MaisVendidos DESC;
+
+-- LISTE OS CLIENTES INFORMANDO
+-- TOTAL GASTO, MEDIA DE PRODUTOS, DATA DO PRIMEIRO E ULTIMO PEDIDO
+
+SELECT 
+pedido.clienteId,
+COUNT(*) AS NumPedidos,
+SUM(produto.preco * itenspedido.quantidade) AS TotalGasto,
+AVG (itenspedido.quantidade) AS MediaProdutosPorPedido,
+MIN(pedido.dataPedido) AS Primeiro_Pedido,
+MAX(pedido.dataPedido) AS Ultimo_Pedido
+FROM pedido
+JOIN itenspedido ON pedido.id = itenspedido.pedidoId
+JOIN produto ON itenspedido.produtoId = produto.id
+GROUP BY pedido.clienteId
