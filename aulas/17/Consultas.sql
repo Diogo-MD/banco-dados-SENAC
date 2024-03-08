bd097a16bf83e739320eebfe3204085c4f23a9a7
Diogo-MD <diogomdossantos@gmail.com>
2024-03-07 21:06:47 -0300

    Atualização Sql

---
+++ aulas/17/Consultas.sql
@@ -0,0 +1,11 @@
+# Consultas sem JOINS
+
+-- Consulte todos os Clientes de São Paulo (SP)
+
+-- Selecionar hospedagens com mais de 3 pessoas e desconto igual a 0
+
+-- Selecionar telefones de clientes com ID 1 ou 2, limitando a 2 resultados
+
+-- Selecionar chalés com capacidades entre 4 e 6, ordenados por valor em alta estação
+
+-- Selecione todos os itens que contenham "de" na descrição, pulando os 2 primeiros resultados
