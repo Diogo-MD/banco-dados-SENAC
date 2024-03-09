1317c9e8cc1353609e2b20588a9b750ca36e2207
Diogo-MD <diogomdossantos@gmail.com>
2024-03-08 21:52:35 -0300

    Atualização Aula 18

---
+++ aulas/18/having_view_index.sql
@@ -0,0 +1,44 @@
+# Having
+
+-- Selecionar Chalés com média de capacidade superior a 3
+SELECT codChale, AVG(capacidade) as capacidade_media
+FROM chale
+GROUP BY codChale
+Having capacidade_media > 3;
+
+-- Selecione os clientes com ao menos uma hospedagem registrada
+
+SELECT codCliente, COUNT(*) AS qtd_hospedagens
+FROM hospedagem
+GROUP BY codCliente
+HAVING qtd_hospedagens > 1;
+
+-- Selecionar Serviços com valor total superior a 40
+
+SELECT hospedagem_servico.codServico, SUM(servico.valorServico) AS total_valor
+FROM hospedagem_servico
+JOIN servico ON hospedagem_servico.codServico = servico.codServico
+GROUP BY hospedagem_servico.codHospedagem
+HAVING total_valor > 40;
+
+
+-- Criar uma VIEW
+CREATE VIEW ViewMediaCapacidade AS 
+SELECT codChale, AVG(capacidade) AS media_capacidade
+FROM chale
+GROUP BY codChale;
+
+-- Selecionar dados da VIEW
+	SELECT * FROM ViewMediaCapacidade;
+    
+-- Crie uma VIEW que informe aos dados da hospedagem, com os dados do cliente, incluindo o telefone
+	
+    SELECT * FROM hospedagem;
+    
+    CREATE VIEW ViewHospedagemDadosClientes AS
+	SELECT Cliente.*, GROUP_CONCAT(telefone.telefone) AS Telefone
+	FROM Cliente
+	LEFT JOIN Telefone ON Cliente.codCliente = Telefone.codCliente
+	GROUP BY Cliente.codCliente;
+    
+    SELECT * FROM ViewHospedagemDadosClientes
